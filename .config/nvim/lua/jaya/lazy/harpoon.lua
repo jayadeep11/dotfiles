4e3970c647e8e6062496a3acb09ca35ddbfa30a6
jayadeep11 <jayadeep.bellamkonda@gmail.com>
2024-08-24 19:05:26 +0530

    ..

--- .config/nvim/lua/jaya/lazy/harpoon.lua
+++ .config/nvim/lua/jaya/lazy/harpoon.lua
@@ -5,18 +5,7 @@ return {
 		local ui = require("harpoon.ui")
 		local mark = require("harpoon.mark")
 
-		local navigate = function(id)
-			return function()
-				ui.nav_file(id)
-			end
-		end
-
 		vim.keymap.set("n", "mm", ui.toggle_quick_menu, { desc = "Open Harpoon" })
 		vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "[A]dd file to Harpoon" })
-		vim.keymap.set("n", "<leader>1", navigate(1), { desc = "Harpoon Buffer (1)" })
-		vim.keymap.set("n", "<leader>2", navigate(2), { desc = "Harpoon Buffer (2)" })
-		vim.keymap.set("n", "<leader>3", navigate(3), { desc = "Harpoon Buffer (3)" })
-		vim.keymap.set("n", "<leader>4", navigate(4), { desc = "Harpoon Buffer (4)" })
-		vim.keymap.set("n", "<leader>5", navigate(5), { desc = "Harpoon Buffer (5)" })
 	end,
 }
---
+++ .config/nvim/lua/jaya/lazy/oil.lua
@@ -0,0 +1,9 @@
+return {
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		-- Optional dependencies
+		dependencies = { "echasnovski/mini.icons" },
+		dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+	},
+}

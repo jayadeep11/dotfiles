ce66510cded81ce878d81a8af0d7924d74fe317b
jayadeep11 <jayadeep.bellamkonda@gmail.com>
2024-12-06 17:04:16 +0530

    ...

--- .config/.nvim/lua/jaya/lazy/oil.lua
+++
@@ -1,9 +0,0 @@
-return {
-	{
-		"stevearc/oil.nvim",
-		opts = {},
-		-- Optional dependencies
-		dependencies = { "echasnovski/mini.icons" },
-		dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-	},
-}
